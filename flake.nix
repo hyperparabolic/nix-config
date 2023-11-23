74eaa9aa961f9a447efae7a24ffe779e13d1376e
Spencer Balogh <spbalogh@gmail.com>
2023-11-23 04:30:35 +0000

    nix-shell for bootstrapping and sops stuff
    includes support for bootstrapping on a raspberry pi later

--- flake.nix
+++ flake.nix
@@ -19,9 +19,35 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+    pkgsPlatform = nixpkgs.lib.genAttrs supportedSystems (system: import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    });
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
+    # bootstrapping and repo tooling
+    devShells = forAllSystems (system:
+      let
+        pkgs = pkgsPlatform.${system};
+      in {
+        default = pkgs.mkShell {
+          #name = "devTooling";
+          #NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+          buildInputs = with pkgs; [
+            nix
+            git
+            sops
+            ssh-to-age
+            gnupg
+            age
+          ];
+        };
+    });
+
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#oak'
     nixosConfigurations = {
@@ -31,15 +57,19 @@
       };
     };
 
+    # standalone home-manager is kinda borked with my config?
+    # maybe dig at this later, but just gets invoked via nxios-rebuild
+    # for now
+
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#spencer@oak'
-    homeConfigurations = {
-      "spencer@oak" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./home/oak.nix];
-      };
-    };
+    #homeConfigurations = {
+    #  "spencer@oak" = home-manager.lib.homeManagerConfiguration {
+    #    pkgs = pkgsPlatform.x86_64-linux;
+    #    extraSpecialArgs = {inherit inputs outputs;};
+    #    modules = [./home/oak.nix];
+    #  };
+    #};
   };
 }
 
