efa2e1903bb81cdce8fa563d3f82faaaf5d4e7f0
Spencer Balogh <spbalogh@gmail.com>
2023-11-26 22:36:43 +0000

    also use home-manager unstable

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:Mic92/sops-nix";
