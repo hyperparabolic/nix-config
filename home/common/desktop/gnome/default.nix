881049bfad251a56f4993d76c84f1f856f1ecbce
Spencer Balogh <spbalogh@gmail.com>
2023-11-28 01:42:03 -0600

    gnome default behaviors

--- home/common/desktop/gnome/default.nix
+++ home/common/desktop/gnome/default.nix
@@ -3,4 +3,29 @@
     ./extensions.nix
     ./theme.nix
   ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "org.gnome.Nautilus.desktop"
+        "org.gnome.Console.desktop"
+        "firefox.desktop"
+        "slack.desktop"
+        "discord.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      enable-hot-corners = false;
+    };
+    
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      # do not automatically suspend
+      sleep-inactive-ac-type = "nothing";
+    };
+  };
 }
