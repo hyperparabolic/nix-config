19d573c20132fce12a2b43cdb712519427348ec5
Spencer Balogh <spbalogh@gmail.com>
2023-11-28 05:42:55 +0000

    dark mode and add gnome theme

---
+++ home/common/desktop/gnome/theme.nix
@@ -0,0 +1,52 @@
+{ pkgs, ... }: {
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    theme = {
+      name = "Orchis";
+      package = pkgs.orchis-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  home.sessionVariables.GTK_THEME = "Orchis";
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+
+      enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+      ];
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Orchis";
+    };
+  };
+
+  home.packages = with pkgs; [
+    gnomeExtensions.user-themes
+    orchis-theme
+  ];
+}
