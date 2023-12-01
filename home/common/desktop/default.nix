d57bc94c21d4dc7fa2ed198478f23bcff70dbe5f
Spencer Balogh <spbalogh@gmail.com>
2023-11-30 22:59:08 -0600

    install gnome-randr and wlr-randr
    one of these will work as a xrandr replacement on most wayland desktops.

--- home/common/desktop/default.nix
+++ home/common/desktop/default.nix
@@ -1,8 +1,14 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./discord.nix
     ./firefox.nix
     ./slack.nix
     ./vscode.nix
+  ];
+
+  home.packages = with pkgs; [
+    # wayland xrandr tools
+    gnome-randr
+    wlr-randr
   ];
 }
