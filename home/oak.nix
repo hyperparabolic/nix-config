9158dda5e5eac02c62e0ae7a6e68572e49d996ae
Spencer Balogh <spbalogh@gmail.com>
2023-12-02 17:02:33 -0600

    add systemd managed symlink to tank/src dataset to home directory

--- home/oak.nix
+++ home/oak.nix
@@ -4,4 +4,9 @@
     ./common/desktop
     ./common/desktop/gnome
   ];
+
+  systemd.user.tmpfiles.rules = [
+    # format: https://www.freedesktop.org/software/systemd/man/latest/tmpfiles.d.html
+    "L /home/spencer/src - - - - /tank/src"
+  ];
 }
