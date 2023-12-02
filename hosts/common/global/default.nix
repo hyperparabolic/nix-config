ffa85c2976fe90dfb8b53b7322a6c9e18ef3b7e0
Spencer Balogh <spbalogh@gmail.com>
2023-12-02 15:40:20 -0600

    increase file handles limit for sudoers

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -13,4 +13,20 @@
   };
 
   time.timeZone = lib.mkDefault "America/Chicago";
+
+  # increase file handle limits for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
 }
