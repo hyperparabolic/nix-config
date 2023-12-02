212af6752f0ed5968023faf66c729ff51f9e64b1
Spencer Balogh <spbalogh@gmail.com>
2023-12-02 14:19:57 -0600

    another trusted key

--- hosts/common/users/spencer.nix
+++ hosts/common/users/spencer.nix
@@ -8,6 +8,7 @@
       extraGroups = ["wheel"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICuxIDOWjjLv2g/Pnr0/V+NtlvFfGadJq5Cxsb06lQ1X spencer@sloth"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHbkshQXiKpkxbyYe+H6duIbCblGSws5jwP//g8zhq7bWCVQSPWo8I7lJbmqlaqUINnjQWTZXMlKuH6g7NihsUY= spencer@dugong"
       ];
       hashedPasswordFile = config.sops.secrets.spencer-password.path;
     };
@@ -18,3 +19,4 @@
     neededForUsers = true;
   };
 }
+
