2ce1a5a583f8b315579af49dab92a05fcf7b89f0
Spencer Balogh <spbalogh@gmail.com>
2023-12-11 14:31:18 -0600

    remove tmpfs src mount
    This misbehaves due to home directory ownership issues when using podman
    with alternate storage directories. Just scrapping it to avoid different
    behavior depending on how you navigate to that directory.

--- home/oak.nix
+++ home/oak.nix
@@ -4,9 +4,4 @@
     ./common/desktop
     ./common/desktop/gnome
   ];
-
-  systemd.user.tmpfiles.rules = [
-    # format: https://www.freedesktop.org/software/systemd/man/latest/tmpfiles.d.html
-    "L /home/spencer/src - - - - /tank/src"
-  ];
 }
