e7358635536414325364bb2343112b704f2b2a77
Spencer Balogh <spbalogh@gmail.com>
2023-12-11 20:51:49 -0600

    init redbud - still needs more bootstrapping tweaks
    still TODO:
    - generate hardware-configuration.nix in installer.
    - work on home manager after first boot.
    - disable all usages of sops-nix and re-enable after first boot after
      age keys are generated and available to re-encrypt everything.

---
+++ hosts/redbud/configuration.nix
@@ -0,0 +1,61 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    inputs.impermanence.nixosModules.impermanence
+    ./hardware-configuration.nix
+    ../common/global
+    ../common/optional/gnome.nix
+    ../common/optional/pipewire.nix
+    ../common/users/spencer.nix
+  ];
+
+  nixpkgs = {
+    # host specific overlays
+    overlays = [];
+  };
+
+  networking.hostName = "redbud";
+
+  # required for ZFS
+  networking.hostId = "55fbb629";
+
+  programs = {
+    dconf.enable = true;
+  };
+
+  boot = {
+    kernelParams = [
+      # no swap, disable hibernate
+      "nohibernate"
+    ];
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
+  };
+
+  hyperparabolic.base.zfs = {
+    enable = true;
+    autoSnapshot = false; # TODO: configure and enable later
+    rollbackSnapshot = "rpool/crypt/local/root@blank";
+    # zedMailTo = "root"; # value doesn't matter, not using email, just needs to not be null;
+    # zedMailCommand = "${pkgs.notify}/bin/notify";
+    # zedMailCommandOptions = "-bulk -provider-config /run/secrets/notify-provider-config";
+  };
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+}
