6ace396c975416f92d5bcf0fdac9cb3d56e87f06
Spencer Balogh <spbalogh@gmail.com>
2024-01-13 13:32:52 -0600

    hyprland - basic config with binds
    Quite a few more TODOs, I want to get more of the desktop done first
    though.

--- home/common/desktop/hyprland/default.nix
+++ home/common/desktop/hyprland/default.nix
@@ -1,6 +1,31 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [
     inputs.anyrun.homeManagerModules.default
     ./anyrun.nix
+    ./hyprland.nix
   ];
+
+  home.packages = with pkgs; [
+    hyprpicker
+    wl-clipboard
+    wl-screenrec
+    wlogout
+    wlsunset
+  ];
+
+  # wayland compat
+  home.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_SESSION_TYPE = "wayland";
+  };
+
+  # fake a tray to let apps start
+  # https://github.com/nix-community/home-manager/issues/2064
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
 }
---
+++ home/common/desktop/hyprland/hyprland.nix
@@ -0,0 +1,99 @@
+{ config, ... }: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$MOD" = "SUPER";
+      # mostly wayland compat environment variables
+      env = ''
+        XDG_SESSION_TYPE,wayland
+        XDG_SESSION_DESKTOP,Hyprland
+
+        GDK_BACKEND,wayland
+
+        MOZ_DISABLE_RDD_SANDBOX,1
+        MOZ_ENABLE_WAYLAND,1
+
+        OZONE_PLATFORM,wayland
+      '';
+      # startup programs
+      exec-once = [
+
+      ];
+      general = {
+        # TODO: system specific monitor configs
+        border_size = 1;
+        # TODO: border colors
+        gaps_in = 5;
+        gaps_out = 5;
+        layout = "dwindle";
+        no_cursor_warps = true;
+      };
+      # TODO: decoration, animation
+      input = {
+        # rebind caps lock to hyper
+        kb_options = "caps:hyper";
+        # mouse settings
+        accel_profile = "flat";
+        sensitivity = 0.0;
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
+      misc = {
+        animate_mouse_windowdragging = true;
+        # `hyprctl reload` to reload
+        disable_autoreload = true;
+        vrr = 1;
+      };
+      # TODO: multiple monitor keybinds on desktop
+      bind = [
+        "$MOD, Escape, exec, wlogout -p layer-shell"
+
+        "$MOD, R, exec, anyrun"
+
+        "$MOD, Q, killactive"
+        "$MODSHIFT, Q, exit"
+        "$MOD, F, fullscreen"
+        "$MOD, Space, togglefloating"
+        "$MOD, P, pseudo"
+        "$MOD, G, togglesplit"
+        "$MODSHIFT, Space, workspaceopt, allfloat"
+        "$MODSHIFT, P, workspaceopt, allpseudotile"
+
+        "$MOD, Tab, swapnext"
+        "$MODSHIFT, Tab, swapnext, prev"
+
+        "$MOD, K, movefocus, u"
+        "$MOD, J, movefocus, d"
+        "$MOD, L, movefocus, r"
+        "$MOD, H, movefocus, l"
+
+        "$MODCTRL, K, swapwindow, u"
+        "$MODCTRL, J, swapwindow, d"
+        "$MODCTRL, L, swapwindow, r"
+        "$MODCTRL, H, swapwindow, l"
+
+        "$MODSHIFT, K, movewindow, u"
+        "$MODSHIFT, J, movewindow, d"
+        "$MODSHIFT, L, movewindow, r"
+        "$MODSHIFT, H, movewindow, l"
+
+        "$MOD, mouse_down, workspace, e-1"
+        "$MOD, mouse_up, workspace, e+1"
+        "${builtins.concatStringsSep "\n" (builtins.genList (x: let
+            ws = let c = (x + 1) / 10; in builtins.toString (x + 1 - (c * 10));
+          in ''
+            bind = $MOD, ${ws}, workspace, ${toString (x + 1)}
+            bind = $MODSHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}
+          '')
+          10)}"
+      ];
+      bindm = ["$MOD, mouse:272, movewindow" "$MOD, mouse:273, resizewindow"];
+      # TODO: layers
+      xwayland.force_zero_scaling = true;
+    };
+
+
+  };
+}
