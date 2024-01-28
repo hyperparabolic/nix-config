384bef235708b509ea5a67a18d1e768ed5e1e41d
Spencer Balogh <spbalogh@gmail.com>
2024-01-28 14:10:34 -0600

    ags - add systemd services for monitoring swayidle and wlsunset
    Actually integrating directly with dbus is hard. Writing some servies
    that echo the status of swayidle and wlsunset services to files so file
    contents can just be watched instead.

--- home/common/desktop/hyprland/ags.nix
+++ home/common/desktop/hyprland/ags.nix
@@ -14,12 +14,44 @@
       Unit = {
         Description = "ags desktop widgets";
         PartOf = "graphical-session.target";
+        Wants = [ "swayidle-status.service" "wlsunset-status.service" ];
       };
       Service = {
         Environment = "PATH=/run/current-system/sw/bin/";
         ExecStart = "${inputs.ags.packages.x86_64-linux.agsWithTypes}/bin/ags -b hypr";
       };
       Install.WantedBy = [ "hyprland-session.target" ];
+    };
+
+    # Integrating with dbus is hard, watching a file is easy and cheap.
+    # The following servies report up / down statuses of other systemd
+    # services to temporary files to allow monitoring of those serives
+    # for UI based controls.
+    swayidle-status = {
+      Unit = {
+        Description = "swayidle service status - file monitoring";
+        PartOf = "swayidle.service";
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils-full}/bin/echo 0 > /tmp/swayidle-status'";
+        ExecStop = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils-full}/bin/echo 1 > /tmp/swayidle-status'";
+        RemainAfterExit = true;
+      };
+      Install.WantedBy = [ "swayidle.service" ];
+    };
+    wlsunset-status = {
+      Unit = {
+        Description = "wlsunset service status - file monitoring";
+        PartOf = "wlsunset.service";
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils-full}/bin/echo 0 > /tmp/wlsunset-status'";
+        ExecStop = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils-full}/bin/echo 1 > /tmp/wlsunset-status'";
+        RemainAfterExit = true;
+      };
+      Install.WantedBy = [ "wlsunset.service" ];
     };
   };
 }
