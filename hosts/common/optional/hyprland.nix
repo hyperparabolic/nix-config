6048e7b41407c3d4dd35fce0ab1cdf79a54f1211
Spencer Balogh <spbalogh@gmail.com>
2024-05-17 19:53:47 -0500

    hyprland - add gnome polkit agent service

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -24,7 +24,24 @@
 
   security = {
     pam.services.swaylock.text = "auth include login";
+    polkit.enable = true;
     rtkit.enable = true;
+  };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
   };
 
   xdg.portal = {
