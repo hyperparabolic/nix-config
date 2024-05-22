7237c16ac863bbf6a55a1c45c3adf6d159eabc54
Spencer Balogh <spbalogh@gmail.com>
2024-05-22 15:04:53 -0500

    helix - add yaml-language-server

--- home/common/helix/languages.nix
+++ home/common/helix/languages.nix
@@ -3,33 +3,39 @@
   lib,
   ...
 }: {
-  programs.helix.languages = {
-    language-server = {
-      bash-language-server = {
-        command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
-      };
+  programs.helix = {
+    extraPackages = with pkgs; [
+      yaml-language-server
+    ];
 
-      nil = {
-        command = lib.getExe pkgs.nil;
-        config.nil.formatting.command = ["${lib.getExe pkgs.alejandra}" "-q"];
-      };
-
-      rust-analyzer = {
-        config.rust-analyzer = {
-          cargo.loadOutDirsFromCheck = true;
-          checkOnSave.command = "clippy";
-          procMacro.enable = true;
-          lens = {
-            references = true;
-            methodReferences = true;
-          };
-          completion.autoimport.enable = true;
-          experimental.procAttrMacros = true;
+    languages = {
+      language-server = {
+        bash-language-server = {
+          command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
         };
-      };
 
-      typescript-language-server = {
-        command = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server";
+        nil = {
+          command = lib.getExe pkgs.nil;
+          config.nil.formatting.command = ["${lib.getExe pkgs.alejandra}" "-q"];
+        };
+
+        rust-analyzer = {
+          config.rust-analyzer = {
+            cargo.loadOutDirsFromCheck = true;
+            checkOnSave.command = "clippy";
+            procMacro.enable = true;
+            lens = {
+              references = true;
+              methodReferences = true;
+            };
+            completion.autoimport.enable = true;
+            experimental.procAttrMacros = true;
+          };
+        };
+
+        typescript-language-server = {
+          command = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server";
+        };
       };
     };
   };
