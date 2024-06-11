e37635afbc56920a81b4734b64e6aeef5956210b
Spencer Balogh <spbalogh@gmail.com>
2024-06-11 17:36:26 -0500

    greetd - also support user defined wayland sessions
    Cannot use regreet's environment variables config for XDG_DATA_DIRS
    as these environment variables are not loaded until after regreets
    config and sessions are loaded.

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -5,6 +5,8 @@
   ...
 }: let
   homeCfg = config.home-manager.users.spencer;
+  homeSharePath = "${homeCfg.home.path}/share";
+  vars = "XDG_DATA_DIRS=$XDG_DATA_DIRS:${homeSharePath}";
 in {
   users.extraUsers.greeter = {
     packages = [
@@ -24,13 +26,20 @@ in {
         icon_theme_name = homeCfg.gtk.iconTheme.name;
         theme_name = homeCfg.gtk.theme.name;
       };
+      env = {
+        XDG_DATA_DIRS = homeSharePath;
+      };
     };
   };
 
   services.greetd = {
     enable = true;
-    settings= {
-      default_session.command = "${lib.getExe pkgs.cage} -- ${lib.getExe config.programs.regreet.package}";
+    settings = {
+      default_session.command = "${lib.getExe (
+        pkgs.writeShellScriptBin "regreet-home-sessions" ''
+          ${vars} ${lib.getExe pkgs.cage} -- ${lib.getExe config.programs.regreet.package}
+        ''
+      )}";
       initial_session = {
         command = "Hyprland";
         user = "spencer";
