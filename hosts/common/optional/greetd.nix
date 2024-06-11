dad7d07354a9f8802c556ba059305e46fe291320
Spencer Balogh <spbalogh@gmail.com>
2024-06-11 01:38:43 -0500

    greetd - proper logout support with regreeter
    Still starts the hyprland session without auth on boot (all systems
    require drive decryption with a stronger password), but support logging
    out to regreeter to lock down access to virtual terminals.
    
    Also supports switching to an alternate session type.

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,15 +1,40 @@
 {
-  services.greetd = let
-    session = {
-      command = "Hyprland";
-      user = "spencer";
-    };
-  in {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  homeCfg = config.home-manager.users.spencer;
+in {
+  users.extraUsers.greeter = {
+    packages = [
+      pkgs.cage
+      homeCfg.gtk.theme.package
+      homeCfg.gtk.iconTheme.package
+    ];
+    home = "/tmp/greeter-home";
+    createHome = true;
+  };
+
+  programs.regreet = {
     enable = true;
     settings = {
-      terminal.vt = 1;
-      default_session = session;
-      initial_session = session;
+      GTK = {
+        application_prefer_dark_theme = true;
+        icon_theme_name = homeCfg.gtk.iconTheme.name;
+        theme_name = homeCfg.gtk.theme.name;
+      };
+    };
+  };
+
+  services.greetd = {
+    enable = true;
+    settings= {
+      default_session.command = "${lib.getExe pkgs.cage} -- ${lib.getExe config.programs.regreet.package}";
+      initial_session = {
+        command = "Hyprland";
+        user = "spencer";
+      };
     };
   };
 
