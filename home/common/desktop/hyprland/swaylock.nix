e2688c209dbf5537dce4ce709804caa3c2a673e1
Spencer Balogh <spbalogh@gmail.com>
2024-08-26 20:19:31 -0500

    trimming down swaylock config
    fade-in has stopped working correctly after an update, just disabling
    some of the config to have less moving parts.

--- home/common/desktop/hyprland/swaylock.nix
+++ home/common/desktop/hyprland/swaylock.nix
@@ -1,19 +1,11 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
     settings = {
       clock = true;
-      color = "#000000df";
       effect-pixelate = "15";
       effect-vignette = "0.3:0";
-      grace = 5;
-      fade-in = 5;
-      font = config.fontProfiles.regular.family;
       screenshots = true;
     };
   };
