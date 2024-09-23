f6366f9d1e01a215dcdd43344b6f5fbac5c16f36
Spencer Balogh <spbalogh@gmail.com>
2024-09-22 19:54:17 -0500

    tailscale - temporarily roll back to stable package

--- hosts/common/global/tailscale.nix
+++ hosts/common/global/tailscale.nix
@@ -1,8 +1,14 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   services.tailscale = {
     enable = true;
     openFirewall = true;
     useRoutingFeatures = lib.mkDefault "client";
+    # temporarily roll back tailscale, tailscale networking is failing for 1.74.0 after 2 reboots
+    package = pkgs.stable.tailscale;
   };
 
   environment.persistence = {
