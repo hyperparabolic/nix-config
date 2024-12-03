38886628c504a50c1c5f8507bb753b5dba6a0fef
Spencer Balogh <spbalogh@gmail.com>
2024-12-02 22:50:55 -0600

    update flake inputs
    - also switch tailscale back to unstable package
    - also migrate to namespaced nerd fonts

--- home/common/desktop/font.nix
+++ home/common/desktop/font.nix
@@ -3,7 +3,7 @@
     enable = true;
     monospace = {
       family = "FiraCode Nerd Font";
-      package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+      package = pkgs.nerd-fonts.fira-code;
     };
     regular = {
       family = "Fira Sans";
