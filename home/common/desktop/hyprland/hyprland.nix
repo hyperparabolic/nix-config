39489aaab1acdad7324445d28176ad96f213cf5c
Spencer Balogh <spbalogh@gmail.com>
2025-01-14 20:02:27 -0600

    hyprland - reverse workspace init order

--- home/common/desktop/hyprland/hyprland.nix
+++ home/common/desktop/hyprland/hyprland.nix
@@ -22,13 +22,14 @@ in {
       exec-once = [
         # "persistent" workspaces linger, but only after they've been visited once.
         # iterate expected workspaces on startup.
-        "hyprctl dispatch workspace 8"
-        "hyprctl dispatch workspace 7"
-        "hyprctl dispatch workspace 6"
-        "hyprctl dispatch workspace 5"
-        "hyprctl dispatch workspace 4"
-        "hyprctl dispatch workspace 3"
+        "hyprctl dispatch workspace 1"
         "hyprctl dispatch workspace 2"
+        "hyprctl dispatch workspace 3"
+        "hyprctl dispatch workspace 4"
+        "hyprctl dispatch workspace 5"
+        "hyprctl dispatch workspace 6"
+        "hyprctl dispatch workspace 7"
+        "hyprctl dispatch workspace 8"
         "hyprctl dispatch workspace 1"
 
         "wl-paste --type text --watch cliphist store"
