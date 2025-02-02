117f174263e579998a39163f46515d557949cf9b
Spencer Balogh <spbalogh@gmail.com>
2025-02-01 18:40:25 -0600

    flake cleanup - re-write systems / packages handling

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    systems.url = "github:nix-systems/default-linux";
 
     ags.url = "github:Aylur/ags";
 
@@ -35,29 +36,29 @@
   outputs = {
     self,
     nixpkgs,
+    home-manager,
+    systems,
     ...
   } @ inputs: let
     inherit (self) outputs;
-
-    supportedSystems = ["x86_64-linux" "aarch64-linux"];
-
-    pkgsPlatform = nixpkgs.lib.genAttrs supportedSystems (system:
-      import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      });
-
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    lib = nixpkgs.lib // home-manager.lib;
+    pkgsFor = lib.genAttrs (import systems) (
+      system:
+        import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+    );
+    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
   in {
+    inherit lib;
     nixosModules = import ./modules/nixos;
     homeManagerModules = import ./modules/home-manager;
 
     overlays = import ./overlays {inherit inputs outputs;};
 
     # bootstrapping and repo tooling
-    devShells = forAllSystems (system: let
-      pkgs = pkgsPlatform.${system};
-    in {
+    devShells = forEachSystem (pkgs: {
       default = pkgs.mkShell {
         NIX_CONFIG = "extra-experimental-features = nix-command flakes";
         buildInputs = with pkgs; [
@@ -74,19 +75,19 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#oak'
     nixosConfigurations = {
-      magnolia = nixpkgs.lib.nixosSystem {
+      magnolia = lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./hosts/magnolia/configuration.nix];
       };
-      oak = nixpkgs.lib.nixosSystem {
+      oak = lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./hosts/oak/configuration.nix];
       };
-      redbud = nixpkgs.lib.nixosSystem {
+      redbud = lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./hosts/redbud/configuration.nix];
       };
-      warden = nixpkgs.lib.nixosSystem {
+      warden = lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./hosts/warden/configuration.nix];
       };
