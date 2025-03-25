9dba5687dcc701e8fe38968f4fa0aaf1144f8506
Spencer Balogh <spbalogh@gmail.com>
2025-03-25 18:35:04 -0500

    ssh - deprecate rsa keys
    I don't have any tooling that relies on these anymore.

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -29,11 +29,6 @@ in {
         path = "${lib.optionalString hasPersistDir "/persist"}/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }
-      {
-        path = "${lib.optionalString hasPersistDir "/persist"}/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
     ];
   };
 
