5ab24e628e2f1e77c7b9a76b551e81922b17eb36
Spencer Balogh <spbalogh@gmail.com>
2025-04-11 14:03:36 -0500

    secureboot - add input lanzaboote and optional config for it
    secureboot is enabled and enforced on magnolia now.

---
+++ hosts/common/optional/secureboot.nix
@@ -0,0 +1,26 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    inputs.lanzaboote.nixosModules.lanzaboote
+  ];
+
+  environment.systemPackages = [
+    pkgs.sbctl
+  ];
+
+  # replaces systemd-boot
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+
+  environment.persistence."/persist".directories = [
+    "/var/lib/sbctl"
+  ];
+}
