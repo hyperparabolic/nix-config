f333bbf8baac9743c4b26e1e0942f78f733ff213
Spencer Balogh <spbalogh@gmail.com>
2025-05-15 14:28:12 -0500

    warden - reprovision as zfsOnLuks and rotate keys

--- hosts/warden/configuration.nix
+++ hosts/warden/configuration.nix
@@ -95,6 +95,10 @@
       enable = true;
       autoSnapshot = false; # TODO: configure and enable later
       impermanenceRollbackSnapshot = "rpool/crypt/local/root@blank";
+      luksOnZfs = {
+        enable = true;
+        backingDevices = ["dev-nvme0n1p1.device"];
+      };
       zedMailTo = "root"; # value doesn't matter, not using email, just needs to not be null;
       zedMailCommand = "${pkgs.notify}/bin/notify";
       zedMailCommandOptions = "-bulk -provider-config /run/secrets/notify-provider-config";
