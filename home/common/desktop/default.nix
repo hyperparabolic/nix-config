07a6118bffe0767159c3b1a869276006e57e7e4c
Spencer Balogh <spbalogh@gmail.com>
2025-08-12 17:23:22 -0500

    chore(cleanup) - cleanup stylix IFD

--- home/common/desktop/default.nix
+++ home/common/desktop/default.nix
@@ -25,4 +25,10 @@
     ++ lib.map lazy-app.override [
       {pkg = d-spy;}
     ];
+
+  stylix.image = pkgs.fetchurl {
+    name = "leaves.png";
+    url = "https://i.imgur.com/9EAejof.png";
+    hash = "sha256-k3nLKUCmjrUAkM7NJev/LNSCC5Kx6jWBXyngAb/MZXU=";
+  };
 }
--- home/common/stylix.nix
+++ home/common/stylix.nix
@@ -2,12 +2,26 @@
   stylix = {
     enable = true;
     autoEnable = false;
-    image = ../../wallpaper/leaves.png;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    override = {
+    base16Scheme = {
+      name = "Catppuccin Macchiato (Yellow)";
+      author = "https://github.com/catppuccin/catppuccin";
+      base00 = "24273a";
+      base01 = "1e2030";
+      base02 = "363a4f";
+      base03 = "494d64";
+      base04 = "5b6078";
+      base05 = "cad3f5";
+      base06 = "f4dbd6";
+      base07 = "b7bdf8";
+      base08 = "ed8796";
+      base09 = "f5a97f";
       base0A = "8aadf4";
+      base0B = "a6da95";
+      base0C = "8bd5ca";
       base0D = "eebb77";
+      base0E = "c6a0f6";
+      base0F = "f0c6c6";
     };
 
     cursor = {
