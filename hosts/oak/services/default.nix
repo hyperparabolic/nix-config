5f968316e5a22fd56cacfde3f7919f0c30218c2c
Spencer Balogh <spbalogh@gmail.com>
2025-08-16 01:55:03 -0500

    feature - add immich wip
    This is mostly good to go, but things are behind in the derivation.
    Immich migrated to vectorchord in place of pgvecto-rs, and that seems
    to be what opened up postgres 17 support. There's already a WIP PR
    for this migration: https://github.com/NixOS/nixpkgs/pull/428568
    
    Once this is merged, a few more changes are necessary after user /
    service are created:
    - Uncomment mediaLocation
    - Create zfs dataset for postgres database, migrate initialized data
    into that dataset and enable snapshots.

--- hosts/oak/services/default.nix
+++ hosts/oak/services/default.nix
@@ -3,6 +3,7 @@
     ../../common/optional/nginx.nix
     ./acme.nix
     ./hydra.nix
+    ./immich.nix
     ./jellyfin.nix
     ./kavita.nix
     ./miniflux.nix
---
+++ hosts/oak/services/immich.nix
@@ -0,0 +1,25 @@
+{config, ...}: {
+  services = {
+    immich = {
+      enable = true;
+      # TODO: user must exist first and fix permissions
+      # mediaLocation = "/tank/immich";
+      settings.server.externalDomain = "https://imgs.oak.decent.id";
+    };
+    nginx.virtualHosts."imgs.oak.decent.id" = {
+      # allow large downloads / uploads
+      extraConfig = ''
+        client_max_body_size 1000M;
+        proxy_read_timeout 600s;
+        proxy_send_timeout 600s;
+        send_timeout       600s;
+      '';
+      forceSSL = true;
+      useACMEHost = "oak.decent.id";
+      locations."/" = {
+        proxyPass = "http://localhost:${toString config.services.immich.port}";
+        proxyWebsockets = true;
+      };
+    };
+  };
+}
