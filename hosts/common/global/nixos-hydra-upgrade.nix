74a688f833b8c87ee871f255716344e35c9607ce
Spencer Balogh <spbalogh@gmail.com>
2025-10-01 11:03:52 -0500

    nixos-hydra-upgrade - ensure trust settings with systemd oneshot

--- hosts/common/global/nixos-hydra-upgrade.nix
+++ hosts/common/global/nixos-hydra-upgrade.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   system.autoUpgradeHydra = {
@@ -22,11 +23,25 @@
       nixos-rebuild = {
         operation = lib.mkDefault "boot";
         host = lib.mkDefault config.networking.hostName;
-        args = [
-          # root user doesn't always have trusted-settings.json, and I trust my repos
-          "--accept-flake-config"
-        ];
       };
     };
+  };
+
+  # I don't run nixos-rebuild as the root user when used manually,
+  # ensure root user has trusted-settings.json
+  systemd.services.ensure-trusted-settings = {
+    description = "Ensure trusted-settings.json trusts oak";
+    wantedBy = ["nixos-hydra-upgrade.service"];
+    before = ["nixos-hydra-upgrade.service"];
+    path = with pkgs; [
+      jq
+    ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      if [ ! -f /root/.local/share/nix/trusted-settings.json ]; then
+          echo "{}" > /root/.local/share/nix/trusted-settings.json
+      fi
+      echo "$(jq '. += {"extra-substituters":{"https://cache.oak.decent.id":true},"extra-trusted-public-keys":{"cache.oak.decent.id:rf560rkaTPzpc8cg56bnPmmgqro8Lbn624jJSDF5YyY=":true}}' /root/.local/share/nix/trusted-settings.json)" > /root/.local/share/nix/trusted-settings.json
+    '';
   };
 }
