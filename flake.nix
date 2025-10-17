1e277890addcf4256b78d28a8221c6bbbc13c57f
Spencer Balogh <spbalogh@gmail.com>
2025-10-16 20:22:48 -0500

    feat - add nix-diff to dev shell
    diffing tool for derivations

--- flake.nix
+++ flake.nix
@@ -99,6 +99,7 @@
         NIX_CONFIG = "extra-experimental-features = nix-command flakes";
         buildInputs = with pkgs; [
           nix
+          nix-diff
           git
           sops
           ssh-to-age
