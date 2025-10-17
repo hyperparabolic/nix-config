ffe702b78eb4c4fc1eb63fef5c8f4b2a270337a2
Spencer Balogh <spbalogh@gmail.com>
2025-10-17 13:08:42 -0500

    chore - refactor hydraJobs with pipe operator
    Easier to read. Significant reduction to parentheses mess.

--- hydra.nix
+++ hydra.nix
@@ -4,15 +4,13 @@
 }: let
   inherit (inputs.nixpkgs) lib;
 in {
-  hosts = lib.pipe outputs.nixosConfigurations [
-    # remove t iso, I don't need it every build
-    (lib.filterAttrs
-      (n: _v: !builtins.elem n ["iso"]))
-    (lib.mapAttrs
-      (_: cfg: (
-        # add to my jobs dashboard and set up evaluation error notifications
-        lib.addMetaAttrs {maintainers = ["email@ntfy.decent.id"];}
-        cfg.config.system.build.toplevel
-      )))
-  ];
+  hosts =
+    outputs.nixosConfigurations
+    # remove iso, I don't need it every build
+    |> lib.filterAttrs (n: _v: !builtins.elem n ["iso"])
+    # add to my jobs dashboard and set up evaluation error notifications
+    |> lib.mapAttrs (
+      _: cfg:
+        lib.addMetaAttrs {maintainers = ["email@ntfy.decent.id"];} cfg.config.system.build.toplevel
+    );
 }
