658e6d2772c9c51f12b42a2ae36282df647ae9ac
Spencer Balogh <spbalogh@gmail.com>
2025-10-27 20:15:09 -0500

    chore - remove legacyModules/monitors.nix, deprecated

--- home/magnolia.nix
+++ home/magnolia.nix
@@ -5,25 +5,6 @@
     ./common/games
   ];
 
-  monitors = [
-    {
-      name = "eDP-1";
-      width = 2256;
-      height = 1504;
-      primary = true;
-      workspaces = [
-        "1"
-        "2"
-        "3"
-        "4"
-        "5"
-        "6"
-        "7"
-        "8"
-      ];
-    }
-  ];
-
   home.persistence."/persist".directories = ["src"];
 
   # suspend after 6 minutes
--- home/oak.nix
+++ home/oak.nix
@@ -1,36 +1,6 @@
-{lib, ...}: {
+{
   imports = [
     ./common
     ./common/desktop/hyprland
-  ];
-
-  monitors = [
-    {
-      name = "HDMI-A-1";
-      width = 3840;
-      height = 2160;
-      x = 0;
-      primary = true;
-      workspaces = [
-        "1"
-        "2"
-        "5"
-        "6"
-      ];
-    }
-    {
-      name = "DVI-D-1";
-      width = 1920;
-      height = 1080;
-      x = 3840;
-      # vertical orientation
-      transform = 3;
-      workspaces = [
-        "3"
-        "4"
-        "7"
-        "8"
-      ];
-    }
   ];
 }
