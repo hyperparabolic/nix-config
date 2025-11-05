1331f3b78693f8f5e8c4cb766e08f7d9ba543191
Spencer Balogh <spbalogh@gmail.com>
2025-11-04 19:03:25 -0600

    chore - migrate unrecognized non-free firmware import to core module
    I don't know if this is needed. I don't want to find out on oak first.
    Will test removing this somewhere else first.

--- hosts/redbud/hardware-configuration.nix
+++ hosts/redbud/hardware-configuration.nix
@@ -4,13 +4,8 @@
 {
   config,
   lib,
-  modulesPath,
   ...
 }: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
   boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
