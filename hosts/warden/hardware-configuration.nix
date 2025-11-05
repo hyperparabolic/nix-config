e1968f93bcb1f8369fb1acb47b7d0c739dba1315
Spencer Balogh <spbalogh@gmail.com>
2025-11-04 20:03:42 -0600

    feat(dendritic) - migrate warden fs config to hosts-warden

--- hosts/warden/hardware-configuration.nix
+++ hosts/warden/hardware-configuration.nix
@@ -11,19 +11,6 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/A419-5A72";
-    fsType = "vfat";
-    options = ["umask=0077"];
-  };
-
-  swapDevices = [
-    {
-      device = "/dev/disk/by-partuuid/9186936a-6547-4506-8e49-74e40caebd04";
-      randomEncryption.enable = true;
-    }
-  ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
