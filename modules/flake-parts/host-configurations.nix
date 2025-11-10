6fc26e29c592dc4d58839626d3ffd211605d1d1b
Spencer Balogh <spbalogh@gmail.com>
2025-11-10 14:42:25 -0600

    feat - generate nixosConfigurations from hosts directory structure

--- modules/flake-parts/host-configurations.nix
+++ modules/flake-parts/host-configurations.nix
@@ -3,37 +3,19 @@
   inputs,
   config,
   ...
-}: {
-  flake = let
-    inherit (inputs.nixpkgs) lib;
-    inherit (self) outputs;
-  in {
-    nixosConfigurations = let
-      specialArgs = {inherit inputs outputs;};
-    in {
-      # TODO: generate programmatically from directory or module structure
-      magnolia = lib.nixosSystem {
-        inherit specialArgs;
-        modules = config.flake.modules.nixos.hosts-magnolia.imports;
-      };
-      oak = lib.nixosSystem {
-        inherit specialArgs;
-        modules = config.flake.modules.nixos.hosts-oak.imports;
-      };
-      redbud = lib.nixosSystem {
-        inherit specialArgs;
-        modules = config.flake.modules.nixos.hosts-redbud.imports;
-      };
-      warden = lib.nixosSystem {
-        inherit specialArgs;
-        modules = config.flake.modules.nixos.hosts-warden.imports;
-      };
-
-      # iso debugging / bootstrapping
-      iso = lib.nixosSystem {
-        inherit specialArgs;
-        modules = config.flake.modules.nixos.hosts-iso.imports;
-      };
-    };
-  };
+}: let
+  inherit (inputs.nixpkgs) lib;
+  inherit (self) outputs;
+  specialArgs = {inherit inputs outputs;};
+  hostModulePrefix = "hosts-";
+  hosts =
+    builtins.readDir "${self}/modules/hosts"
+    |> lib.filterAttrs (_n: v: v == "directory")
+    |> builtins.attrNames;
+in {
+  flake.nixosConfigurations = lib.genAttrs hosts (name:
+    lib.nixosSystem {
+      inherit specialArgs;
+      modules = config.flake.modules.nixos."${hostModulePrefix}${name}".imports;
+    });
 }
