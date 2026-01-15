1e04cfd084586ad7bd21e7e6821b466b95a8b1c0
Spencer Balogh <spbalogh@gmail.com>
2026-01-15 17:30:44 -0600

    feat - zellij alt+tab tab switching

--- modules/core/cli/zellij.nix
+++ modules/core/cli/zellij.nix
@@ -8,6 +8,22 @@
         scroll_buffer_size = 4294967295;
         show_release_notes = true;
         show_startup_tips = true;
+
+        keybinds = {
+          "shared_except \"locked\"" = {
+            _children = [
+              {
+                bind = {
+                  _args = ["Alt Tab"];
+                  _children = [
+                    {GoToNextTab = {};}
+                    {SwitchToMode._args = ["normal"];}
+                  ];
+                };
+              }
+            ];
+          };
+        };
       };
     };
 
