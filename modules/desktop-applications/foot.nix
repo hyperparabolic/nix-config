66c971b77f629d20aaaa5c9a2e6e1f529eab968a
Spencer Balogh <spbalogh@gmail.com>
2026-01-15 23:12:14 -0600

    feat - foot config
    Just needs a small amount of padding to keep things looking sane with
    rounded corners. Otherwise going to try configuring everything in zellij
    first.

--- modules/desktop-applications/foot.nix
+++ modules/desktop-applications/foot.nix
@@ -3,6 +3,10 @@
     programs.foot = {
       enable = true;
       settings = {
+        main = {
+          # ${x_pad}x${y_pad}
+          pad = "5x1 center-when-maximized-and-fullscreen";
+        };
       };
     };
 
