c11703fc8a5151e013f06cf0db9a27a184bcb2c7
Spencer Balogh <spbalogh@gmail.com>
2026-01-23 22:25:39 -0600

    feat - update nixos-hydra-upgrade and migrate config
    Also adding newer trusted-settings. None of these warnings / errors are
    ever encountered here as they are caught in hydra. Still, reduces some
    log noise around untrusted settings.

--- modules/core/nixos-hydra-upgrade.nix
+++ modules/core/nixos-hydra-upgrade.nix
@@ -21,14 +21,14 @@
           jobset = "main";
           job = lib.mkDefault "hosts.${config.networking.hostName}";
         };
-        nixos-rebuild = {
+        nix_build = {
           operation = lib.mkDefault "boot";
           host = lib.mkDefault config.networking.hostName;
         };
       };
     };
 
-    # I don't run nixos-rebuild as the root user when used manually,
+    # I don't run nix as the root user when used manually,
     # ensure root user has trusted-settings.json
     systemd.services.ensure-trusted-settings = {
       description = "Ensure trusted-settings.json trusts oak";
@@ -42,7 +42,7 @@
         if [ ! -f /root/.local/share/nix/trusted-settings.json ]; then
             echo "{}" > /root/.local/share/nix/trusted-settings.json
         fi
-        echo "$(jq '. += {"extra-substituters":{"https://cache.oak.decent.id":true},"extra-trusted-public-keys":{"cache.oak.decent.id:rf560rkaTPzpc8cg56bnPmmgqro8Lbn624jJSDF5YyY=":true}}' /root/.local/share/nix/trusted-settings.json)" > /root/.local/share/nix/trusted-settings.json
+        echo "$(jq '. += {"abort-on-warn":{"true":true},"allow-import-from-derivation":{"false":true},"extra-experimental-features":{"pipe-operators":true},"extra-substituters":{"https://cache.oak.decent.id":true},"extra-trusted-public-keys":{"cache.oak.decent.id:rf560rkaTPzpc8cg56bnPmmgqro8Lbn624jJSDF5YyY=":true}}' /root/.local/share/nix/trusted-settings.json)" > /root/.local/share/nix/trusted-settings.json
       '';
     };
   };
