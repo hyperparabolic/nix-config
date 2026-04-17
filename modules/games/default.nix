ed24771b1337e3d6f1b71a434f0804cee9646c7f
Spencer Balogh <spbalogh@gmail.com>
2026-04-17 13:34:43 -0500

    feat(games) - celeste mods

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -6,10 +6,18 @@
     };
   };
 
-  flake.modules.homeManager.games = {config, ...}: {
+  flake.modules.homeManager.games = {
+    config,
+    pkgs,
+    ...
+  }: {
     home = {
+      packages = with pkgs; [
+        (olympus.override {celesteWrapper = "steam-run";})
+      ];
       persistence."${config.this.impermanence.dirs.games}".directories = [
         "Games"
+        ".config/Olympus"
         ".factorio"
         ".local/share/Steam"
         ".local/share/Tabletop Simulator"
