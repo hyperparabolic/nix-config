decdf41baacde7498e250210d38637becc8d4489
Spencer Balogh <spbalogh@gmail.com>
2026-04-29 19:12:33 -0500

    feat(zellij) - hide status bar by default, keybinds to enable / disable

--- modules/core/cli/zellij.nix
+++ modules/core/cli/zellij.nix
@@ -5,6 +5,7 @@
       enableFishIntegration = true;
       settings = {
         copy_command = "wl-copy";
+        default_layout = "disable-status-bar";
         scroll_buffer_size = 4294967295;
         show_release_notes = false;
         show_startup_tips = false;
@@ -20,6 +21,34 @@
                   _children = [
                     {GoToNextTab = {};}
                     {SwitchToMode._args = ["normal"];}
+                  ];
+                };
+              }
+              {
+                bind = {
+                  _args = ["Ctrl Shift /"];
+                  _children = [
+                    {
+                      OverrideLayout._children = [
+                        {layout._args = ["default"];}
+                        {retain_existing_terminal_panes._args = [true];}
+                        {apply_only_to_active_tab._args = [true];}
+                      ];
+                    }
+                  ];
+                };
+              }
+              {
+                bind = {
+                  _args = ["Ctrl /"];
+                  _children = [
+                    {
+                      OverrideLayout._children = [
+                        {layout._args = ["disable-status-bar"];}
+                        {retain_existing_terminal_panes._args = [true];}
+                        {apply_only_to_active_tab._args = [true];}
+                      ];
+                    }
                   ];
                 };
               }
