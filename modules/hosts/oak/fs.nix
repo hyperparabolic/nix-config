60f8f5581f3f1f5536d127361f738437dd02f23a
Spencer Balogh <spbalogh@gmail.com>
2026-06-06 15:05:16 -0500

    oak - add some bind mounts into tank to home directory

--- modules/hosts/oak/fs.nix
+++ modules/hosts/oak/fs.nix
@@ -51,25 +51,75 @@
     # there are some issues with non-legacymount datasets imported via boot.zfs.extraPools
     # mainly that pools imported this way mount all datasets including canmount=noauto
     # I don't want these mounted on boot, so this service mounts them instead
-    systemd.services.zfs-mount-tank = {
-      description = "import tank zfs pool and mount datasets";
-      enable = true;
-      # early enough in boot, sidesteps issues with hardware being unavailable
-      wantedBy = ["multi-user.target"];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecCondition = lib.getExe (
-          # ensure pool is not already mounted
-          pkgs.writeShellScriptBin "zfs-check-tank-mount" ''
-            ! ${config.boot.zfs.package}/bin/zpool status | grep 'pool: tank$'
-          ''
-        );
-        ExecStart = lib.getExe (
-          pkgs.writeShellScriptBin "zfs-tank-mount" ''
-            ${config.boot.zfs.package}/bin/zpool import tank
-            ${config.boot.zfs.package}/bin/zfs mount -a
-          ''
-        );
+    systemd = {
+      # bind mounts from home into tank for convenience
+      mounts = [
+        {
+          what = "/tank/net/books";
+          where = "/home/spencer/books";
+          options = "bind";
+          type = "none";
+          unitConfig.DefaultDependencies = "no";
+          wants = [
+            "user@1000.service"
+            "zfs-mount-tank.service"
+          ];
+          after = [
+            "user@1000.service"
+            "zfs-mount-tank.service"
+          ];
+        }
+        {
+          what = "/tank/net/src";
+          where = "/home/spencer/src";
+          options = "bind";
+          type = "none";
+          unitConfig.DefaultDependencies = "no";
+          wants = [
+            "user@1000.service"
+            "zfs-mount-tank.service"
+          ];
+          after = [
+            "user@1000.service"
+            "zfs-mount-tank.service"
+          ];
+        }
+        {
+          what = "/tank/net/tabs";
+          where = "/home/spencer/tabs";
+          options = "bind";
+          type = "none";
+          unitConfig.DefaultDependencies = "no";
+          wants = [
+            "user@1000.service"
+            "zfs-mount-tank.service"
+          ];
+          after = [
+            "user@1000.service"
+            "zfs-mount-tank.service"
+          ];
+        }
+      ];
+      services.zfs-mount-tank = {
+        description = "import tank zfs pool and mount datasets";
+        enable = true;
+        # early enough in boot, sidesteps issues with hardware being unavailable
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecCondition = lib.getExe (
+            # ensure pool is not already mounted
+            pkgs.writeShellScriptBin "zfs-check-tank-mount" ''
+              ! ${config.boot.zfs.package}/bin/zpool status | grep 'pool: tank$'
+            ''
+          );
+          ExecStart = lib.getExe (
+            pkgs.writeShellScriptBin "zfs-tank-mount" ''
+              ${config.boot.zfs.package}/bin/zpool import tank
+              ${config.boot.zfs.package}/bin/zfs mount -a
+            ''
+          );
+        };
       };
     };
   };
