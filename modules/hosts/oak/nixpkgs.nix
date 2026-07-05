8d06e9b7a68c0de81697307fc28fcb47706bbc47
Spencer Balogh <spbalogh@gmail.com>
2026-07-04 22:24:49 -0500

    oak(nixpkgs) - override onnxruntime build to limit to 8 cores
    OOM'd in hydra. Maybe could be tuned up, after looking at metrics next
    week. This is good for sure.

--- modules/hosts/oak/nixpkgs.nix
+++ modules/hosts/oak/nixpkgs.nix
@@ -2,41 +2,42 @@
   flake.modules.nixos.hosts-oak = {...}: {
     nixpkgs.config = {
       cudaSupport = true;
-      # experimenting with enabling CPU optimizations, disabled for now
-      #
-      # packageOverrides = pkgs: {
-      #   # build for oak CPU / GPU capabilities. Performance improvements,
-      #   # but this is not portable to other hosts.
-      #   llama-cpp =
-      #     (pkgs.llama-cpp.override {
-      #       blasSupport = true;
-      #       cudaSupport = true;
-      #       metalSupport = false;
-      #       rocmSupport = false;
-      #     }).overrideAttrs (oldAttrs: {
-      #       cmakeFlags =
-      #         (oldAttrs.cmakeFlags or [])
-      #         ++ [
-      #           # enable CPU optimizations
-      #           "-DGGML_NATIVE=ON"
-      #           # speed up builds, only build for my cuda arch
-      #           "-DCMAKE_CUDA_ARCHITECTURES=86"
-      #         ];
-      #       preConfigure = ''
-      #         # don't suppress -march=native
-      #         export NIX_ENFORCE_NO_NATIVE=0
-      #         ${oldAttrs.preConfigure or ""}
-      #       '';
-      #     });
-      #   onnxruntime = pkgs.onnxruntime.overrideAttrs (oldAttrs: {
-      #     # RAM use building this package with optimizations is
-      #     # wild. -j8 to avoid slowdown due to RAM limits.
-      #     preBuild = ''
-      #       NIX_BUILD_CORES=8
-      #       ${oldAttrs.preBuild or ""}
-      #     '';
-      #   });
-      # };
+      packageOverrides = pkgs: {
+        # experimenting with enabling CPU optimizations, disabled for now,
+        # core dumps for some models at the moment.
+        #
+        # build for oak CPU / GPU capabilities. Performance improvements,
+        # but this is not portable to other hosts.
+        # llama-cpp =
+        #   (pkgs.llama-cpp.override {
+        #     blasSupport = true;
+        #     cudaSupport = true;
+        #     metalSupport = false;
+        #     rocmSupport = false;
+        #   }).overrideAttrs (oldAttrs: {
+        #     cmakeFlags =
+        #       (oldAttrs.cmakeFlags or [])
+        #       ++ [
+        #         # enable CPU optimizations
+        #         "-DGGML_NATIVE=ON"
+        #         # speed up builds, only build for my cuda arch
+        #         "-DCMAKE_CUDA_ARCHITECTURES=86"
+        #       ];
+        #     preConfigure = ''
+        #       # don't suppress -march=native
+        #       export NIX_ENFORCE_NO_NATIVE=0
+        #       ${oldAttrs.preConfigure or ""}
+        #     '';
+        #   });
+        onnxruntime = pkgs.onnxruntime.overrideAttrs (oldAttrs: {
+          # RAM use building this package with optimizations is
+          # wild. -j8 to avoid slowdown due to RAM limits.
+          preBuild = ''
+            NIX_BUILD_CORES=8
+            ${oldAttrs.preBuild or ""}
+          '';
+        });
+      };
     };
   };
 }
