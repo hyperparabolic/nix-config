28574ed3d89ff7dab3e718732dadf126a17d0adf
Spencer Balogh <spbalogh@gmail.com>
2026-07-18 12:28:59 -0500

    chore - remove vesktop, relies on EoL electron

--- modules/desktop-applications/discord.nix
+++ modules/desktop-applications/discord.nix
@@ -2,14 +2,10 @@
   flake.modules.homeManager.desktop-applications = {pkgs, ...}: {
     home.packages = [
       pkgs.discord
-      pkgs.vesktop
     ];
-
-    stylix.targets.vesktop.enable = true;
 
     home.persistence."/persist".directories = [
       ".config/discord"
-      ".config/vesktop"
     ];
   };
 }
