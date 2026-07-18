e89ee87db341271b35bfcef80c46e380669e3d80
Spencer Balogh <spbalogh@gmail.com>
2026-07-18 13:18:44 -0500

    fix - workaround for broken patool / bottles tests

--- modules/flake-parts/overlays.nix
+++ modules/flake-parts/overlays.nix
@@ -4,5 +4,23 @@
     lazy-app = final: _: {
       lazy-app = inputs.lazy-apps.packages.${final.stdenv.hostPlatform.system}.lazy-app;
     };
+
+    # TODO: remove once PR 540742 is merged
+    # Work around too strict landlock hardening breaking patool tests used in bottles
+    patool-tests = final: prev: {
+      python314Packages = prev.python314Packages.overrideScope (
+        pyFinal: pyPrev: {
+          patool = pyPrev.patool.override {
+            file = prev.file.overrideAttrs {
+              postPatch = ''
+                substituteInPlace src/landlock.c --replace-fail \
+                  "LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR" \
+                  "LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR | LANDLOCK_ACCESS_FS_EXECUTE"
+              '';
+            };
+          };
+        }
+      );
+    };
   };
 }
