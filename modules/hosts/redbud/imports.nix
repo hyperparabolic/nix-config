30b0f89a1b727fbf4ed1f00d99dc40f228a1c663
Spencer Balogh <spbalogh@gmail.com>
2026-08-08 10:03:43 -0500

    fix - brain fart, wrong host

--- modules/hosts/redbud/imports.nix
+++ modules/hosts/redbud/imports.nix
@@ -15,6 +15,15 @@
         secureboot
         user-spencer
         zfs
+
+        # TODO: tpm decrypt failure
+        # automated LUKS unlocks are borked, no time to dig atm
+        # clues for looking later:
+        # `tpm2_getcap properties-variable`
+        # maybe related?:
+        # https://github.com/systemd/systemd/issues/42725
+        # https://github.com/systemd/systemd/commit/be8a7b418a3493ecaa1b7f36abcbd8685aa2931d
+        stage1-ssh
       ]
       ++ [
         {
