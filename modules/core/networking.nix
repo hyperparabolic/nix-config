5b4d56e31c312d1527b60c0d1dfcbe5b54727ea4
Spencer Balogh <spbalogh@gmail.com>
2026-08-23 16:44:35 -0500

    feat(core) - switch to systemd networkd backed networking by default

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,11 +1,18 @@
 {
   flake.modules.nixos.core = {lib, ...}: {
-    networking.networkmanager.enable = true;
+    networking = {
+      networkmanager.enable = true;
+      useNetworkd = true;
+    };
     services.resolved = {
       enable = lib.mkDefault true;
       settings = {
         Resolve.FallbackDNS = [];
       };
+    };
+    systemd.network = {
+      enable = true;
+      wait-online.enable = lib.mkDefault false;
     };
   };
 }
