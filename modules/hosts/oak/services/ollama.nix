611015585807413f50d7670f207ec0e0ebcb4b95
Spencer Balogh <spbalogh@gmail.com>
2026-08-23 19:31:24 -0500

    chore - remove unused ollama module

--- modules/hosts/oak/services/ollama.nix
+++
@@ -1,9 +0,0 @@
-{
-  flake.modules.nixos.hosts-oak = {pkgs, ...}: {
-    services.ollama = {
-      enable = true;
-      package = pkgs.ollama-vulkan;
-    };
-    environment.persistence."/persist".directories = ["/var/lib/private/ollama"];
-  };
-}
