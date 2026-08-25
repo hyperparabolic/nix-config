ea47ec19ecce0d755edae1c4e6030ddd5f0477e0
Spencer Balogh <spbalogh@gmail.com>
2026-08-24 20:16:31 -0500

    feat(pi) - remove optionally disabling gondolin
    I was playing around with some other ideas for sandboxing, including
    running pi sandboxed in a nixos microvm. I didn't really like how they
    worked in practice. Removing the ability to disable gondolin for now.

--- modules/pi/default.nix
+++ modules/pi/default.nix
@@ -1,82 +1,62 @@
 {
-  flake.modules.homeManager.pi = {
-    config,
-    lib,
-    pkgs,
-    ...
-  }: let
-    cfg = config.this.pi;
+  flake.modules.homeManager.pi = {pkgs, ...}: let
   in {
-    options.this.pi = {
-      gondolin-sandbox = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = "Whether to enable the gondolin sandbox extension";
-      };
-    };
+    home.packages = with pkgs; [
+      # gondolin image build deps
+      cpio
+      lz4
+      e2fsprogs
+    ];
 
-    config = {
-      home.packages = with pkgs; [
-        # gondolin image build deps
-        cpio
-        lz4
-        e2fsprogs
+    programs.pi-coding-agent = {
+      enable = true;
+      extraPackages = [
       ];
-
-      programs.pi-coding-agent = {
-        enable = true;
-        extraPackages = [
-        ];
-        context = "Be a good bot.";
-        models = {
-          providers = {
-            llama-swap = {
-              baseUrl = "https://llm.oak.decent.id/v1";
-              api = "openai-responses";
-              apiKey = "dummy";
-              models = [
-                {id = "gemma-4:31b-q4";}
-                {id = "qwen3.6:27b-q4";}
-                {id = "deepseek/deepseek-v4-flash";}
-                {id = "nvidia/nemotron-3-ultra-550b-a55b:free";}
-                {id = "stealth/ox-alpha";}
-                {id = "tencent/hy3";}
-                {id = "xiaomi/mimo-v2.5";}
-              ];
-            };
+      context = "Be a good bot.";
+      models = {
+        providers = {
+          llama-swap = {
+            baseUrl = "https://llm.oak.decent.id/v1";
+            api = "openai-responses";
+            apiKey = "dummy";
+            models = [
+              {id = "gemma-4:31b-q4";}
+              {id = "qwen3.6:27b-q4";}
+              {id = "deepseek/deepseek-v4-flash";}
+              {id = "nvidia/nemotron-3-ultra-550b-a55b:free";}
+              {id = "stealth/ox-alpha";}
+              {id = "tencent/hy3";}
+              {id = "xiaomi/mimo-v2.5";}
+            ];
           };
         };
-        settings = {
-          defaultModel = "llama-swap/nvidia/nemotron-3-ultra-550b-a55b:free";
-          enabledModels = [
-            "llama-swap/gemma-4:31b-q4"
-            "llama-swap/qwen3.6:27b-q4"
-            "llama-swap/deepseek/deepseek-v4-flash"
-            "llama-swap/nvidia/nemotron-3-ultra-550b-a55b:free"
-            "llama-swap/stealth/ox-alpha"
-            "llama-swap/tencent/hy3"
-            "llama-swap/xiaomi/mimo-v2.5"
-          ];
+      };
+      settings = {
+        defaultModel = "llama-swap/nvidia/nemotron-3-ultra-550b-a55b:free";
+        enabledModels = [
+          "llama-swap/gemma-4:31b-q4"
+          "llama-swap/qwen3.6:27b-q4"
+          "llama-swap/deepseek/deepseek-v4-flash"
+          "llama-swap/nvidia/nemotron-3-ultra-550b-a55b:free"
+          "llama-swap/stealth/ox-alpha"
+          "llama-swap/tencent/hy3"
+          "llama-swap/xiaomi/mimo-v2.5"
+        ];
 
-          extensions =
-            [
-              # always included extensions
-            ]
-            ++ lib.optionals cfg.gondolin-sandbox [
-              (pkgs.buildNpmPackage {
-                pname = "pi-extensions";
-                version = "unstable";
-                src = ./extensions;
-                # npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-                npmDepsHash = "sha256-5eMwGOLbqUyRWxOwZ8tZ2uFabE8y1fs3X4f7HTd2+9w=";
-                dontNpmBuild = true;
-                installPhase = ''
-                  mkdir -p $out
-                  cp -r . $out
-                '';
-              })
-            ];
-        };
+        extensions = [
+          (pkgs.buildNpmPackage {
+            pname = "pi-extensions";
+            version = "unstable";
+            src = ./extensions;
+            # npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            npmDepsHash = "sha256-5eMwGOLbqUyRWxOwZ8tZ2uFabE8y1fs3X4f7HTd2+9w=";
+            dontNpmBuild = true;
+            installPhase = ''
+              mkdir -p $out
+              cp -r . $out
+            '';
+          })
+        ];
       };
     };
   };
